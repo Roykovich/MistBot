a6d1cb768fb49bbacfee4cdc1ed7577effd60f72
Rubén Ortiz <runerd.menethil@gmail.com>
2023-11-08 03:52:39 -0300

    add lavaling password

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ COMMANDS_DIR = BASE_DIR / 'commands'
 COGS_DIR = BASE_DIR / 'cogs'
 
 TOKEN = None
+MUSIC_PASS = None
 
 # ensure that the config file exists
 if os.path.exists('config.json'):
@@ -20,6 +21,13 @@ if os.path.exists('config.json'):
     # Error
     if not TOKEN:
         print('Theres no token in config.json')
+        sys.exit(1)
+
+    # we extract the password of lavalink server
+    MUSIC_PASS = json_dict['lavalink']
+
+    if not MUSIC_PASS:
+        print('Theres no lavalink password in config.json')
         sys.exit(1)
     
     json_file.close()
