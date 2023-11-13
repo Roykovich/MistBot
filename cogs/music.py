ea8ce6ed0165e386a19376ed4c79efa4bf77ff7f
Rubén Ortiz <runerd.menethil@gmail.com>
2023-11-13 00:56:17 -0300

    add views to the music

--- cogs/music.py
+++ cogs/music.py
@@ -17,6 +17,65 @@ def format_time(milliseconds):
 
     return f'{hours}:{minutes:02d}:{seconds:.0f}'
 
+# ! Agregar el view a una variable de la cog para evitar que los comandos
+# ! skip, stop, pause o resume la bugeen, basicamente hacerla una view global
+# ! para poder manejar su estado
+
+class MusicView(discord.ui.View):
+    paused : bool = False
+
+    @discord.ui.button(label='❌')
+    async def stop(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.vc.stop()
+        self.clear_items()
+        await interaction.response.edit_message(view=self)
+
+    @discord.ui.button(label='⏸')
+    async def pause_toggle(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not self.paused:
+            await self.vc.pause()
+            self.children[1].label = '▶️'
+            self.paused = True
+        else:
+            await self.vc.resume()
+            self.children[1].label = '⏸'
+            self.paused = False
+
+        await interaction.response.edit_message(view=self)
+
+    @discord.ui.button(label='⏭️')
+    async def next_song(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.vc.queue.is_empty:
+            await interaction.response.send_message(f'That was the last song')
+            await self.vc.disconnect()
+            for item in self.children:
+                item.disabled = True
+            await interaction.response.edit_message(view=self)
+            return
+
+        next_track = await self.vc.queue.get_wait()
+        await self.vc.play(next_track, populate=False)
+        self.clear_items()
+        await interaction.response.edit_message(view=self)
+
+class LofiView(discord.ui.View):
+    @discord.ui.button(label='Add', style=discord.ButtonStyle.success)
+    async def add(self, interaction: discord.Interaction, button: discord.ui.Button):
+        embed = interaction.message.embeds[0]
+        embed.description = f'Lofi added succesfully to the playlist!'
+        self.vc.queue(self.track)
+        self.clear_items()
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @discord.ui.button(label='Remove', style=discord.ButtonStyle.danger)
+    async def remove(self, interaction: discord.Interaction, button: discord.ui.Button):
+        embed = interaction.message.embeds[0]
+        embed.description = f'Removed. Here comes the lofi'
+        self.vc.queue.clear()
+        await self.vc.play(self.track, populate=False)
+        self.clear_items()
+        await interaction.response.edit_message(embed=embed, view=self)
+
 class Music(commands.Cog):
     vc : wavelink.Player = None
     music_channel = None
@@ -39,19 +98,27 @@ class Music(commands.Cog):
     @commands.Cog.listener()
     async def on_wavelink_track_start(self, payload: wavelink.TrackEventPayload):
         track = payload.track
-        duration = format_time(track.length) if not track.is_stream else '🎙 live'
+        
+        # check if the track is a stream or not
+        duration = format_time(track.length) if not track.is_stream else '🎙 live'\
+        # fetchs thumbnail
         thumbnail = await payload.original.fetch_thumbnail()
+        
         embed = discord.Embed(
             colour = discord.Colour.dark_purple(),
             description = f'[{track.title}]({track.uri})'
         )
-
         embed.set_author(name='🎵 | Suena')
         embed.add_field(name='Duración', value=f'`{duration}`', inline=True)
         embed.add_field(name='autor', value=f'`{track.author}`', inline=True)
         embed.set_thumbnail(url=thumbnail)
 
-        await self.music_channel.send(embed=embed)
+        # check if is stream for the timeout of the buttons
+        view_timeout = track.length / 1000 if not track.is_stream else None 
+        view = MusicView(timeout=view_timeout)
+
+        await self.music_channel.send(embed=embed, view=view)
+        view.vc = self.vc
 
     @commands.Cog.listener()
     async def on_wavelink_track_end(self, payload: wavelink.TrackEventPayload):
@@ -113,14 +180,12 @@ class Music(commands.Cog):
 
         await self.vc.play(track, populate=False)
 
-    
     @commands.command(name='disconnect')
     async def disconnect(self, ctx):
         await self.vc.disconnect()
 
     @commands.command(name='skip')
     async def skip(self, ctx):
-
         if self.vc.queue.is_empty:
             await ctx.send(f'That was the last song')
             await self.vc.disconnect()
@@ -130,13 +195,12 @@ class Music(commands.Cog):
         await self.vc.play(next_track, populate=False)
     
     @commands.command(name='pause')
-    async def pause(self, ctx, a=1, b=2 ,c=3):
-        print(a, b, c)
+    async def pause(self, ctx):
+        await self.vc.pause()
 
     @commands.command(name='resume')
     async def resume(self, ctx):
         await self.vc.resume()
-
 
     @commands.command(name='stop')
     async def stop(self, ctx):
@@ -209,8 +273,17 @@ class Music(commands.Cog):
         track = tracks[0]
 
         if self.vc and self.vc.is_connected():
-            # aqui crear un condicional para ver si se mata
-            # la playlist actual en el caso de que exista
+            embed = discord.Embed(
+                colour = discord.Colour.dark_purple(),
+                description = f'Wanna add the lofi radio to the playlist or you want to remove the playlist and let the radio alone?'
+            )
+            view = LofiView(timeout=15)
+            # this appends the new track to the queue
+            message = await self.music_channel.send(embed=embed, view=view)
+            view.vc = self.vc
+            view.track = track
+            await view.wait()
+            await message.delete(delay=10)
             return
         
         self.vc = await channel.connect(cls=wavelink.Player)
