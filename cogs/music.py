6194cfc7360b2ee15561f00afc9b140e7b1f53b8
Rubén Ortiz <runerd.menethil@gmail.com>
2023-11-14 01:25:35 -0300

    refactor format_time

--- cogs/music.py
+++ cogs/music.py
@@ -15,7 +15,7 @@ def format_time(milliseconds):
     minutes = (milliseconds % 3600000) // 60000
     seconds = ((milliseconds % 3600000) % 60000) / 1000
 
-    return f'{"0" if hours < 10 else ""}{str(hours) + ":" if hours > 0 else ""}{minutes:02d}:{"0" if seconds < 10 else ""}{seconds:.0f}'
+    return f'{"0" if hours < 10 and hours > 0 else ""}{str(hours) + ":" if hours > 0 else ""}{minutes:02d}:{"0" if seconds < 10 else ""}{seconds:.0f}'
 
 def embed_generator(description):
     embed = discord.Embed(
