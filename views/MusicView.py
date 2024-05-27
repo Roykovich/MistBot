ef93dec2b20549f478a9e841e209d77d345c7516
Rubén Ortiz <runerd.menethil@gmail.com>
2024-05-27 02:06:10 -0400

    Add list and peek features and move down a row the next button

--- views/MusicView.py
+++ views/MusicView.py
@@ -1,12 +1,14 @@
 import discord
 from utils.embedGenerator import music_embed_generator
+from views.PlaylistView import PlaylistView
+from utils.formatTime import format_time
 
 
 class MusicView(discord.ui.View):
     paused:bool = False
     skipper:bool = False
 
-    @discord.ui.button(label='Atrasar', emoji='⏪')
+    @discord.ui.button(label='Atr.', emoji='⏪')
     async def backward(self, interaction: discord.Interaction, button: discord.ui.Button):
         new_position = self.vc.position - (15 * 1000)
         await self.vc.seek(new_position)
@@ -19,7 +21,7 @@ class MusicView(discord.ui.View):
         self.clear_items() # clears the buttons
         await interaction.response.edit_message(view=self) # updates the message
 
-    @discord.ui.button(label='Pausar', emoji='⏸️')
+    @discord.ui.button(label='Pausar', emoji='⏸')
     async def pause(self, interaction: discord.Interaction, button: discord.ui.Button):
         # pauses the player if it's playing and changes the button label
         await self.vc.pause(True if not self.paused else False)
@@ -28,13 +30,43 @@ class MusicView(discord.ui.View):
         self.paused = not self.paused
         await interaction.response.edit_message(view=self)
 
-    @discord.ui.button(label='Adelantar', emoji='⏩')
+    @discord.ui.button(label='Adel.', emoji='⏩')
     async def forward(self, interaction: discord.Interaction, button: discord.ui.Button):
         new_position = self.vc.position + (15 * 1000)
         await self.vc.seek(new_position)
         await interaction.response.edit_message(view=self)
-    
-    @discord.ui.button(label='Siguiente', emoji='⏭️')
+
+    @discord.ui.button(label='Lista', emoji='📜', row=1)
+    async def playlist(self, interaction: discord.Interaction, button: discord.ui.Button):
+        view = PlaylistView(timeout=None)
+        view.vc = self.vc
+        view.music_channel = self.music_channel
+        await view.send()
+        await interaction.response.defer()
+
+    @discord.ui.button(label='¿Cuál viene?', emoji='👀', row=1)
+    async def cual(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.vc.queue.is_empty:
+            await interaction.response.send_message(embed=music_embed_generator('No hay ninguna canción'), ephemeral=True)
+            return
+        
+        track = self.vc.queue.peek()
+        current_position = format_time(int(self.vc.position))
+        duration = format_time(track.length) if not track.is_stream else '🎙 live'
+        thumbnail = track.artwork
+
+        embed = discord.Embed(
+            colour = discord.Colour.dark_purple(),
+            description = f'[{track.title}]({track.uri})'
+        )
+        embed.set_author(name='🎵 | Suena')
+        embed.add_field(name='Duración', value=f'`{current_position}/{duration}`', inline=True)
+        embed.add_field(name='autor', value=f'`{track.author}`', inline=True)
+        embed.set_thumbnail(url=thumbnail)
+
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+    @discord.ui.button(label='Siguiente', emoji='⏭️', row=1)
     async def skip(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.vc.queue.is_empty:
             channel = self.vc.channel.mention
