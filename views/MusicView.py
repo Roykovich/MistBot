95cbee53eeeab0389f985e8f89b34039635fb51a
Roykovich <runerd.menethil@gmail.com>
2024-11-04 21:34:48 -0300

    Change buttons of the interface

--- views/MusicView.py
+++ views/MusicView.py
@@ -9,35 +9,65 @@ class MusicView(discord.ui.View):
     paused:bool = False
     skipper:bool = False
 
-    @discord.ui.button(label='Atr.', emoji='<:mb_rewind:1244545665086914660>', style=discord.ButtonStyle.blurple)
-    async def backward(self, interaction: discord.Interaction, button: discord.ui.Button):
-        new_position = self.vc.position - (15 * 1000)
+    # Para atras
+    @discord.ui.button(label='', emoji='<:mb_rewind:1303148087807578173>')
+    async def rewind(self, interaction: discord.Interaction, button: discord.ui.Button):
+        new_position = self.vc.position - (5 * 1000)
         await self.vc.seek(new_position)
         await interaction.response.edit_message(view=self)
 
-    @discord.ui.button(label='Parar', emoji='<:mb_stop:1244545666211119104>', style=discord.ButtonStyle.red)
+
+    @discord.ui.button(label='', emoji='<:mb_back:1244545665086914660>', disabled=True)
+    async def backward(self, interaction: discord.Interaction, button: discord.ui.Button):
+        ...
+
+
+    # pausa y resumir
+    @discord.ui.button(label='', emoji='<:mb_pause:1244545668563861625>', style=discord.ButtonStyle.green)
+    async def pause(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # pauses the player if it's playing and changes the button label
+        await self.vc.pause(True if not self.paused else False)
+        self.children[2].emoji = '<:mb_pause:1244545668563861625>' if self.paused else '<:mb_resume:1244545666982744119>'
+        self.paused = not self.paused
+        await interaction.response.edit_message(view=self)
+
+
+    @discord.ui.button(label='', emoji='<:mb_next:1244545663669243954>')
+    async def skip(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.vc.queue.is_empty:
+            self.clear_items()
+            await interaction.response.edit_message(view=self)
+            await self.vc.stop()
+            return
+
+
+    # Para adelante
+    @discord.ui.button(label='', emoji='<:mb_forward:1303148126193848352>')
+    async def forward(self, interaction: discord.Interaction, button: discord.ui.Button):
+        new_position = self.vc.position + (5 * 1000)
+        await self.vc.seek(new_position)
+        await interaction.response.edit_message(view=self)
+
+
+    @discord.ui.button(label='', emoji='<:mb_loop:1244545660041297930>', row=1, disabled=True)
+    async def loop(self, interaction: discord.Interaction, button: discord.ui.Button):
+        ...
+
+
+    @discord.ui.button(label='', emoji='<:mb_lyrics:1303142190154907658>', row=1, disabled=True)
+    async def lyrics(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message(embed=music_embed_generator(self.lyrics if self.lyrics else 'No se encontraron letras para esta canción'))
+
+
+    @discord.ui.button(label='', emoji='<:mb_stop:1244545666211119104>', style=discord.ButtonStyle.red, row=1)
     async def stop(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.vc.queue.clear() # clears the queue
         await self.vc.stop() # stops the player
         self.clear_items() # clears the buttons
-        await interaction.response.edit_message(view=self) # updates the message
-
-    @discord.ui.button(label='Pausar', emoji='<:mb_pause:1244545668563861625>', style=discord.ButtonStyle.green)
-    async def pause(self, interaction: discord.Interaction, button: discord.ui.Button):
-        # pauses the player if it's playing and changes the button label
-        await self.vc.pause(True if not self.paused else False)
-        self.children[2].label = 'Resumir' if not self.paused else 'Pausar'
-        self.children[2].emoji = '<:mb_pause:1244545668563861625>' if not self.paused else '<:mb_resume:1244545666982744119>'
-        self.paused = not self.paused
         await interaction.response.edit_message(view=self)
 
-    @discord.ui.button(label='Adel.', emoji='<:mb_forward:1244545663669243954>', style=discord.ButtonStyle.blurple)
-    async def forward(self, interaction: discord.Interaction, button: discord.ui.Button):
-        new_position = self.vc.position + (15 * 1000)
-        await self.vc.seek(new_position)
-        await interaction.response.edit_message(view=self)
 
-    @discord.ui.button(label='Lista', emoji='📀', row=1)
+    @discord.ui.button(label='', emoji='<:mb_playlist:1303137502894362634>', row=1)
     async def playlist(self, interaction: discord.Interaction, button: discord.ui.Button):
         view = PlaylistView(timeout=None)
         view.vc = self.vc
@@ -45,31 +75,24 @@ class MusicView(discord.ui.View):
         await view.send()
         await interaction.response.defer()
 
-    # El emoji cuando desbloquees esta feature📜
-    @discord.ui.button(label='Letra', emoji='🔜', row=1)
-    async def lyrics(self, interaction: discord.Interaction, button: discord.ui.Button, disabled: bool=False):
-        await interaction.response.send_message(embed=music_embed_generator(self.lyrics if self.lyrics else 'No se encontraron letras para esta canción'))
+    
+    @discord.ui.button(label='', emoji='<:mb_shuffle:1244545660770979853>', row=1, disabled=True)
+    async def shuffle(self, interaction: discord.Interaction, button: discord.ui.Button):
+        ...
 
-    @discord.ui.button(label='Preview', emoji='👀', row=1)
-    async def preview(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.vc.queue.is_empty:
-            await interaction.response.send_message(embed=music_embed_generator('No hay ninguna canción'), ephemeral=True)
-            return
+    # @discord.ui.button(label='Preview', emoji='👀', row=1)
+    # async def preview(self, interaction: discord.Interaction, button: discord.ui.Button):
+    #     if self.vc.queue.is_empty:
+    #         await interaction.response.send_message(embed=music_embed_generator('No hay ninguna canción'), ephemeral=True)
+    #         return
         
-        track = self.vc.queue.peek()
-        current_position = format_time(int(self.vc.position))
-        embed = now_playing(track, user=self.user_list[0] if self.user_list else None, current=True, position=current_position, peek=True)
+    #     track = self.vc.queue.peek()
+    #     current_position = format_time(int(self.vc.position))
+    #     embed = now_playing(track, user=self.user_list[0] if self.user_list else None, current=True, position=current_position, peek=True)
 
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+    #     await interaction.response.send_message(embed=embed, ephemeral=True)
 
-    @discord.ui.button(label='Siguiente', emoji='<:mb_next:1244545662482255872>', row=1)
-    async def skip(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.vc.queue.is_empty:
-            self.clear_items()
-            await interaction.response.edit_message(view=self)
-            await self.vc.stop()
-            return
-        
+
         await self.vc.play(self.vc.queue.get())
         self.clear_items()
         await interaction.response.edit_message(view=self)
